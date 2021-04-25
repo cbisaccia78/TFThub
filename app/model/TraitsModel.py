34ee5d4afbee98e6b2dea1fe534ae3160557754b
cbisaccia78 <cbisaccia78@gmail.com>
2021-04-25 14:05:51 -0400

    replacing origin and class with trait in accordance with the official TFT api json structure

--- app/model/TraitsModel.py
+++ app/model/TraitsModel.py
@@ -1,6 +1,7 @@
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 
 from app import db
+from app.custom.functions import write_json, read_json
 from app.database.traits import Trait
 
 
@@ -12,20 +13,33 @@ class TraitModel(SQLAInterface):
 
     @staticmethod
     def export_items(items):
-        return
-
-    def export_csv(self, items: list):
-        return
+        data = {"traits": []}
+        for item in items:
+            if not item:
+                continue
+            trait_data = item.to_json()
+            data["traits"].append(trait_data)
+        return data
 
     def export_json(self, items: list):
-        return
+        data = {"name": "traits", "version": appversion()}
+        trait_data = self.export_items(items)
+        data.update(trait_data)
+        outfile = write_json(data)
+        filename = f"Trait-Export.json"
+        return outfile, filename
 
     def import_data(self, data=None):
-        return
+        if data:
+            for trait in data:
+                new_trait = Trait.from_dict(trait)
+                self.add(new_trait)
 
     def import_json(self, json_data):
-        return
+        data = read_json(json_data)
+        self.import_data(data)
 
     @staticmethod
     def get_all():
         return None
+
