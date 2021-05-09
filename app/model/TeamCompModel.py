9dbc086d90e3f7513dd03f3111fb2ffa778fc3b1
cbisaccia78 <cbisaccia78@gmail.com>
2021-05-09 17:42:29 -0400

    pretty stuff

--- app/model/TeamCompModel.py
+++ app/model/TeamCompModel.py
@@ -1,8 +1,7 @@
 from app import db
-from app.custom.functions import write_json, read_json
 from app.database.champions import Champion
-from app.model.ChampionsModel import create_champion_model
-from app.model.TraitsModel import create_trait_model
+from app.model.ChampionsModel import ChampionModel
+from app.model.TraitsModel import TraitModel
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 
 
@@ -11,42 +10,7 @@ def create_team_comp_model():
 
 
 class TeamCompModel(SQLAInterface):
-    trait_datamodel = create_trait_model()
-    champion_datamodel = create_champion_model()
-
-
-    @staticmethod
-    def export_items(items):
-        data = {"champions": []}
-        for item in items:
-            if not item:
-                continue
-            champion_data = item.to_json()
-            data["champions"].append(champion_data)
-        return data
-
-    def export_json(self, items: list):
-        data = {"name": "champions", "version": appversion()}
-        hardware_data = self.export_items(items)
-        data.update(hardware_data)
-        outfile = write_json(data)
-        filename = f"Champion-Export.json"
-        return outfile, filename
-
-    def import_data(self, data=None):
-        if data:
-            for champion in data:
-                new_champ = Champion.from_dict(champion)
-                self.add(new_champ)
-
-    def import_json(self, json_data):
-        data = read_json(json_data)
-        for champion in data.get('champions'):
-            self.origin_datamodel.import_data(champion=champion, data=champion.get('origins'))
-            self.classes_datamodel.import_data(champion=champion, data=champion.get('classes'))
-        self.import_data(data.get('champions'))
 
     @staticmethod
     def get_all():
-        return None
-
+        ChampionModel.
